abd448c12766dde398c618945fb9e88882dfb7db
John D. Hume <duelin.markers@gmail.com>
2009-02-14 16:44:29 -0500

    Add entry form for guestbook entries and "handle" them (by logging them).
    Had an issue with putting a (let for user-service and user inside the defservice but outside the route definitions.
    I think that's supposed to work but will have to research it.
    For now there's just a slightly gross user-info function both "action" functions can call.

--- src/guestbook/servlet/GuestbookServlet.clj
+++ src/guestbook/servlet/GuestbookServlet.clj
@@ -3,26 +3,46 @@
     [compojure.http :as http]
     [compojure.html :as html])
   (:import
-    (com.google.appengine.api.users User UserService UserServiceFactory))
+    (com.google.appengine.api.users User UserService UserServiceFactory)
+    (java.util.logging Logger))
   (:gen-class
     :extends javax.servlet.http.HttpServlet))
 
 
-(defn show-guestbook [user-service user]
-  (html/html
-    [:html
-      [:head [:title "Guestbook"]]
-      [:body
-        (if user
-          [:p (str "Hello, " (.getNickname user) "! (You can ")
-            [:a {:href (.createLogoutURL user-service "/")} "sign out"]
-            ".)"]
-          [:p "Hello! (You can "
-            [:a {:href (.createLoginURL user-service "/")} "sign in"]
-            " to include your name with your greeting when you post.)"])]]))
+(def logger (Logger/getLogger "guestbook.servlet.GuestbookServlet"))
+
+(defn user-info []
+  (let [user-service (UserServiceFactory/getUserService)]
+    [user-service (.getCurrentUser user-service)]))
+
+(defn show-guestbook []
+  (let [[user-service user] (user-info)]
+    (html/html
+      [:html
+        [:head [:title "Guestbook"]]
+        [:body
+          (if user
+            [:p (str "Hello, " (.getNickname user) "! (You can ")
+              [:a {:href (.createLogoutURL user-service "/")} "sign out"]
+              ".)"]
+            [:p "Hello! (You can "
+              [:a {:href (.createLoginURL user-service "/")} "sign in"]
+              " to include your name with your greeting when you post.)"])
+          [:form {:action "/sign" :method "POST"}
+            [:div [:textarea {:name "content" :rows "3" :cols "60"}]]
+            [:div [:input {:type "submit" :value "Post Greeting"}]]]]])))
+
+(defn sign-guestbook [params]
+  (let [[user-service user] (user-info)]
+    (if user
+      (.info logger (str "Greeting posted by " (.getNickname user) ": " (params :content)))
+      (.info logger (str "Greeting posted anonymously: " (params :content))))
+    (http/redirect-to "/")))
 
 (http/defservice "-"
-  (let [user-service (UserServiceFactory/getUserService)
-        user (.getCurrentUser user-service)]
-    (http/GET "/"
-      (show-guestbook user-service user))))
+  (http/POST "/sign"
+    (sign-guestbook params))
+  (http/GET "/"
+    (show-guestbook))
+  (http/ANY "*"
+    [404 "Not found!"]))
