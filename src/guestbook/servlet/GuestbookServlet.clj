8e944d6ad541424393ee7917c008cb562aebf493
John D. Hume <duelin.markers@gmail.com>
2009-02-14 15:45:51 -0500

    HTML UI with login and logout links from the UsersService.

--- src/guestbook/servlet/GuestbookServlet.clj
+++ src/guestbook/servlet/GuestbookServlet.clj
@@ -1,15 +1,28 @@
 (ns guestbook.servlet.GuestbookServlet
-  (:require [compojure.http :as http])
+  (:require
+    [compojure.http :as http]
+    [compojure.html :as html])
   (:import
     (com.google.appengine.api.users User UserService UserServiceFactory))
   (:gen-class
     :extends javax.servlet.http.HttpServlet))
 
 
+(defn show-guestbook [user-service user]
+  (html/html
+    [:html
+      [:head [:title "Guestbook"]]
+      [:body
+        (if user
+          [:p (str "Hello, " (.getNickname user) "! (You can ")
+            [:a {:href (.createLogoutURL user-service "/")} "sign out"]
+            ".)"]
+          [:p "Hello! (You can "
+            [:a {:href (.createLoginURL user-service "/")} "sign in"]
+            " to include your name with your greeting when you post.)"])]]))
+
 (http/defservice "-"
-  (http/GET "/*"
-    (let [user-service (UserServiceFactory/getUserService)
-          user (.getCurrentUser user-service)]
-      (if user
-        [(str "Hello, " (.getNickname user))]
-        (http/redirect-to (.createLoginURL user-service (.getRequestURI request)))))))
+  (let [user-service (UserServiceFactory/getUserService)
+        user (.getCurrentUser user-service)]
+    (http/GET "/"
+      (show-guestbook user-service user))))
