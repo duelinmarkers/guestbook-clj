92ca81478d2199da3485efb9963c75dd4154b0c9
John D. Hume <duelin.markers@gmail.com>
2009-03-22 12:36:51 -0400

    Make the compojure namespaces implicit. This isn't a compojure tutorial.

--- src/guestbook/servlet.clj
+++ src/guestbook/servlet.clj
@@ -1,10 +1,8 @@
 (ns guestbook.servlet
+  (:use
+    compojure.http
+    compojure.html)
   (:require
-    [compojure.http         :as http]
-    [compojure.http.routes  :as routes]
-    [compojure.html.gen     :as html]
-    [compojure.html.form-helpers :as form]
-    [compojure.html.page-helpers :as page]
     [guestbook.greeting     :as greeting]
     [guestbook.clj-exercise :as clj-exercise])
   (:import
@@ -20,18 +18,18 @@
 (defn show-guestbook []
   (let [[user-service user] (user-info)
         greetings (greeting/find-all)]
-    (html/html
+    (html
       [:html
         [:head
           [:title "Guestbook"]
-          (page/include-css "/stylesheets/main.css")]
+          (include-css "/stylesheets/main.css")]
         [:body
           (if user
             [:p "Hello, " (.getNickname user) "! (You can "
-              (page/link-to (.createLogoutURL user-service "/") "sign out")
+              (link-to (.createLogoutURL user-service "/") "sign out")
               ".)"]
             [:p "Hello! (You can "
-              (page/link-to (.createLoginURL user-service "/") "sign in")
+              (link-to (.createLoginURL user-service "/") "sign in")
               " to include your name with your greeting when you post.)"])
           (if (empty? greetings)
             [:p "The guestbook has no messages."]
@@ -42,44 +40,44 @@
                             [:strong (g :author)]
                             "An anonymous guest")
                           " wrote:"]
-                        [:blockquote (g :content)]])
+                        [:blockquote (h (g :content))]])
               greetings))
-          (form/form-to [POST "/sign"]
-            [:div (form/text-area "content" "")]
-            [:div (form/submit-button "Post Greeting")])
-          (page/link-to "/exercise" "exercise clojure a bit")]])))
+          (form-to [POST "/sign"]
+            [:div (text-area "content" "")]
+            [:div (submit-button "Post Greeting")])
+          (link-to "/exercise" "exercise clojure a bit")]])))
 
 (defn sign-guestbook [params]
   (let [[_ user] (user-info)]
     (greeting/create (params :content) (if user (.getNickname user)))
-    (http/redirect-to "/")))
+    (redirect-to "/")))
 
 (defn exercise []
   (let [[_ user] (user-info)
         [atom-value ref-value] (clj-exercise/show-off (if user (.getNickname user) "anon"))]
-    (html/html [:html
+    (html [:html
       [:head
         [:title "Clojure on AppEngine: Atoms and Refs"]
-        (page/include-css "/stylesheets/main.css")]
+        (include-css "/stylesheets/main.css")]
       [:body
         [:h1 "Atoms and Refs"]
         [:p "Each request to this page increments an atom, which starts at zero, 
-            and updates a ref by adding to a vector of visitors and a leaving a timestamp.
+            and updates a ref by adding to a vector of visitors and leaving a timestamp.
             This is just here to illustrate that atoms and refs work.
             But you may see with repeated requests that they work a little strangely
             due to the distributed nature of AppEngine."]
-        [:p "The current atom value is " (html/h atom-value) "."]
-        [:p "The current ref value is " (html/h ref-value) "."]
-        (page/link-to "/" "back to guestbook")]])))
+        [:p "The current atom value is " (h atom-value) "."]
+        [:p "The current ref value is " (h ref-value) "."]
+        (link-to "/" "back to guestbook")]])))
 
-(routes/defroutes guestbook-app
-  (http/POST "/sign"
+(defroutes guestbook-app
+  (POST "/sign"
     (sign-guestbook params))
-  (http/GET "/"
+  (GET "/"
     (show-guestbook))
-  (http/GET "/exercise"
+  (GET "/exercise"
     (exercise))
-  (http/ANY "*"
+  (ANY "*"
     [404 "Not found!"]))
 
-(http/defservice guestbook-app)
+(defservice guestbook-app)
